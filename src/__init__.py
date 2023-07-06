00eb52b2ffc13f8503d029d38bd677336936d37f
AlexKly <Klyukvanstalker@gmail.com>
2023-07-06 18:03:19 +0300

    added new vocabs

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,14 @@ from src.utils.load_txt import load_txt
 _ROOT = os.path.dirname(__file__)
 DIR_VOCABS = f'{_ROOT}/vocabs'
 
-COUNTRIES = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/countries.txt'))
-REGIONS = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/regions.txt'))
-CITIES = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/cities.txt'))
-DISTRICTS = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/districts.txt'))
+COUNTRIES = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/location/countries.txt'))
+REGIONS = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/location/regions.txt'))
+CITIES = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/location/cities.txt'))
+DISTRICTS = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/location/districts.txt'))
+STREETS = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/location/streets.txt'))
+LAST_NAMES_MALE = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/names/last_names_male.txt'))
+LAST_NAMES_FEMALE = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/names/last_names_female.txt'))
+FIRST_NAMES_MALE = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/names/first_names_male.txt'))
+FIRST_NAMES_FEMALE = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/names/first_names_female.txt'))
+MIDDLE_NAMES_MALE = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/names/middle_names_male.txt'))
+MIDDLE_NAMES_FEMALE = marisa_trie.Trie(load_txt(path=f'{DIR_VOCABS}/names/middle_names_female.txt'))
